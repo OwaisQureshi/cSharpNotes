e9c6f96f7d4af069b703be6ee911dbea7eb0861e
Muhammad Owais Qureshi <owaiskureshi@gmail.com>
2020-11-22 23:06:48 -0500

    Added Regex porject

---
+++ cSharpNotes/Regex/1RegexBasics.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Regex
+{
+    public class RegexBasics
+    {
+        /*
+         Quantifier: is a regex operator which matches preceding/forward char/item 0 or 1 times,
+        in other word it says the preceding char 
+        */
+
+    }
+}
